4bd8b954d3e102018a286077afb4f591c0191ec9
Carlos Loitti <carlosloitir@gmail.com>
2024-04-25 17:45:33 -0500

    Tablas DB + Migraciones

---
+++ database/migrations/2024_04_25_224423_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number')->unique();
+            $table->unsignedBigInteger('customer_id');
+            $table->date('date');
+            $table->unsignedBigInteger('pay_mode_id');
+            $table->timestamps();
+
+            // Definir las claves foráneas
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('pay_mode_id')->references('id')->on('pay_mode');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
---
+++ database/migrations/2024_04_25_224448_create_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('invoice_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->timestamps();
+
+            // Definir las claves foráneas
+            $table->foreign('invoice_id')->references('id')->on('invoices');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('details');
+    }
+};
