031396ed2e28887525157c05225e376595b35989
Juan Londoño <carlosloitir@gmail.com>
2024-04-25 20:14:43 -0500

    Facturas List

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -18,12 +19,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 
-    Route::get('/products' , [ProductController::class, 'index'])->name('products.index');
+Route::get('/products' , [ProductController::class, 'index'])->name('products.index');
 Route::post('/products' , [ProductController::class, 'store'])->name('products.store');
 Route::get('/products/create' , [ProductController::class, 'create'])->name('products.create');
 Route::delete('/products/{product}' , [ProductController::class, 'destroy'])->name('products.destroy');
 Route::put('/products/{product}' , [ProductController::class, 'update'])->name('products.update');
 Route::get('/products/{product}/edit' , [ProductController::class, 'edit'])->name('products.edit');
+
+
+Route::get('/invoices' , [InvoiceController::class, 'index'])->name('invoices.index');
+Route::post('/invoices' , [InvoiceController::class, 'store'])->name('invoices.store');
+Route::get('/invoices/create' , [InvoiceController::class, 'create'])->name('invoices.create');
+Route::delete('/invoices/{invoice}' , [InvoiceController::class, 'destroy'])->name('invoices.destroy');
+Route::put('/invoices/{invoice}' , [InvoiceController::class, 'update'])->name('invoices.update');
+Route::get('/invoices/{invoice}/edit' , [InvoiceController::class, 'edit'])->name('invoices.edit');
+
 
 });
 
